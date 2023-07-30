3edfa1d107ad5022a9bcef456ae6e2cd4f00016f
zulker0010 <zulkeryusuf@gmail.com>
2023-07-31 05:33:10 +0600

    scatterplot love vs divorce probability

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth',-1)
@@ -6,3 +7,9 @@ clean_data = pd.read_excel(r'F:\Data Analytics\Divorce probability\Divorce-proba
 
 print(clean_data)
 clean_data.head()
+clean_data.median()
+
+x=clean_data.Love
+y=clean_data.Divorce_Probability
+plt.scatter(x,y)
+plt.show
