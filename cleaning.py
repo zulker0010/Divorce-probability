4882f01b0bec825d9b2c29c508a8a787b8296195
zulker0010 <zulkeryusuf@gmail.com>
2023-08-02 22:52:07 +0600

    split data into training set & testing set

--- cleaning.py
+++ cleaning.py
@@ -18,5 +18,5 @@ plt.show
 
 np.corrcoef(x,y)
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0.2)
+from sklearn.model_selection import train_test_split;
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
